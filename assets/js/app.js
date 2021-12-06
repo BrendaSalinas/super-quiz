82dbf63068451aa3fc932f1afbf196e8cf0a85a7
Brenda Salinas <brenda.salinas2128@gmail.com>
2021-12-05 20:57:27 -0600

    adding the questions and answers

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+//all the questions with the answers using a variable with an array and 5 objects 
+var questions = [
+    
+    {question: "Inside which HTML element do we put the JavaScript", 
+    choices: ["< javascript >", "< scripting >", "< script >", "< js >"],
+    answer: 2
+    },
+
+    {question: "Which of the following is the correct syntax to print a page using JavaScript?", 
+    choices: ["window.print();", "browser.print();", "navigator.print();", "document.print();"],
+    answer: 0
+    },
+
+    {question: "Which of the following function of String object returns the calling string value converted to upper case?", 
+    choices: ["toLocaleUpperCase()", "toUpperCase()", "toString()", "substring()"],
+    answer: 1
+    },
+
+    {question: "The correct sequence of HTML tags for starting a webpage is ", 
+    choices: ["Head, Title, HTML, body", "HTML, Body, Title, Head", "HTML, Head, Body, Div", "HTML, Head, Title, Body"],
+    answer: 3
+    },
+
+    {question: "Which of the following is the correct syntax to select all paragraph elements in a div element?", 
+    choices: ["p", "div p", "div#p", "div~p"],
+    answer: 1
+    },
+
+    {question: "Which of the following tag is used to define options in a drop-down selection list?", 
+    choices: ["< select >", "< list >", "< option >", "< dropdown >"],
+    answer: 2
+    }
+];
+
